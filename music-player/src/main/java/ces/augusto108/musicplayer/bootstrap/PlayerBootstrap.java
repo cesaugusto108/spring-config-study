2ea9dcc280612d68d8926935a1c7ba710b844722
César Augusto <ces.augusto108@gmail.com>
2023-01-08 15:41:51 -0300

    Bootstrap class implementation

--- music-player/src/main/java/ces/augusto108/musicplayer/bootstrap/PlayerBootstrap.java
+++ music-player/src/main/java/ces/augusto108/musicplayer/bootstrap/PlayerBootstrap.java
@@ -1,5 +1,32 @@
 package ces.augusto108.musicplayer.bootstrap;
 
-public class PlayerBootstrap {
+import ces.augusto108.musicplayer.model.Artist;
+import ces.augusto108.musicplayer.model.Playlist;
+import ces.augusto108.musicplayer.model.Song;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlayerBootstrap {
+    public Playlist getPlaylist() {
+        Artist a1 = new Artist("Lighthouse Family");
+        Song s1 = new Song("Lifted", a1);
+
+        Artist a2 = new Artist("Dido");
+        Song s2 = new Song("White flag", a2);
+
+        Artist a3 = new Artist("Back for good");
+        Song s3 = new Song("Take that", a3);
+
+        Playlist p = new Playlist(1);
+
+        Map<Artist, Song> playlist = new HashMap<>();
+        playlist.put(a1, s1);
+        playlist.put(a2, s2);
+        playlist.put(a3, s3);
+
+        p.setPlaylist(playlist);
+
+        return p;
+    }
 }
