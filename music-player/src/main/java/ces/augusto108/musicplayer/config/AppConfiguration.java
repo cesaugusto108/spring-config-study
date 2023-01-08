9a511bece4069d926cb03c361c5702b81aaf036b
César Augusto <ces.augusto108@gmail.com>
2023-01-08 16:53:39 -0300

    Fixed AppConfiguration class to include only one bean (PlayController)

--- music-player/src/main/java/ces/augusto108/musicplayer/config/AppConfiguration.java
+++ music-player/src/main/java/ces/augusto108/musicplayer/config/AppConfiguration.java
@@ -2,7 +2,6 @@ package ces.augusto108.musicplayer.config;
 
 import ces.augusto108.musicplayer.bootstrap.PlayerBootstrap;
 import ces.augusto108.musicplayer.controllers.PlayerController;
-import ces.augusto108.musicplayer.services.PlayerService;
 import ces.augusto108.musicplayer.services.PlayerServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,17 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfiguration {
     @Bean
-    public PlayerBootstrap playerBootstrap() {
-        return new PlayerBootstrap();
-    }
-
-    @Bean
-    public PlayerService playerService() {
-        return new PlayerServiceImpl(playerBootstrap());
-    }
-
-    @Bean
     public PlayerController playerController() {
-        return new PlayerController(playerService());
+        return new PlayerController(new PlayerServiceImpl(new PlayerBootstrap()));
     }
 }
