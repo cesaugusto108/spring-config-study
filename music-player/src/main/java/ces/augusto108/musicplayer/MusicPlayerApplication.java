275806cd35390b3a585fe4f2cbaf07b57fa22086
César Augusto <ces.augusto108@gmail.com>
2023-01-09 10:25:48 -0300

    Bean configuration using xml file

--- music-player/src/main/java/ces/augusto108/musicplayer/MusicPlayerApplication.java
+++ music-player/src/main/java/ces/augusto108/musicplayer/MusicPlayerApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"ces.augusto108.musicplayer.config", "ces.augusto108.musicplayer.controllers"})
+@ComponentScan(basePackages = "ces.augusto108.musicplayer.controllers")
+@ImportResource("classpath:config.xml")
 public class MusicPlayerApplication {
     public static void main(String[] args) {
         ApplicationContext applicationContext = SpringApplication.run(MusicPlayerApplication.class, args);
--- music-player/src/main/java/ces/augusto108/musicplayer/controllers/PlayerController.java
+++ music-player/src/main/java/ces/augusto108/musicplayer/controllers/PlayerController.java
@@ -1,13 +1,14 @@
 package ces.augusto108.musicplayer.controllers;
 
 import ces.augusto108.musicplayer.services.PlayerService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PlayerController {
     private final PlayerService playerService;
 
-    public PlayerController(PlayerService playerService) {
+    public PlayerController(@Qualifier(value = "easyListeningPlayerService") PlayerService playerService) {
         this.playerService = playerService;
     }
 
