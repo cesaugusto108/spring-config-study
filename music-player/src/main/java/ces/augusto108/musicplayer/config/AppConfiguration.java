4d1a05979f09ffd664e17242015e0e4a913e1635
César Augusto <ces.augusto108@gmail.com>
2023-01-10 09:40:40 -0300

    AppConfiguration update: @PropertySource removed (using spring-boot application.properties in the project)

--- music-player/src/main/java/ces/augusto108/musicplayer/config/AppConfiguration.java
+++ music-player/src/main/java/ces/augusto108/musicplayer/config/AppConfiguration.java
@@ -6,10 +6,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource(value = "classpath:application.properties")
 public class AppConfiguration {
     @Bean
     AppProfile appProfile(
